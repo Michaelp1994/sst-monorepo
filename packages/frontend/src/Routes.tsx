9f161527d8dcedca8dfc6431b5a7e71509cb14ec
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-10-25 22:38:10 +1000

    added new note form and update note form

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -3,6 +3,8 @@ import Home from "./containers/Home.tsx";
 import NotFound from "./containers/NotFound.tsx";
 import Login from "./containers/Login.tsx";
 import Signup from "./containers/Signup.tsx";
+import NewNote from "./containers/NewNote.tsx";
+import Notes from "./containers/Notes.tsx";
 
 export default function Links() {
   return (
@@ -11,6 +13,8 @@ export default function Links() {
       <Route path="*" element={<NotFound />} />;
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
+      <Route path="/notes/:id" element={<Notes />} />
     </Routes>
   );
 }
